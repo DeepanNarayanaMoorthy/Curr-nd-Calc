55fdc7cee4f18c98b2d656b133887d68b10e093b
deepannarayanamoorthy <deepan06102000@gmail.com>
2020-06-26 10:54:21 +0530

    command line input works good

--- financebuddy.go
+++ financebuddy.go
@@ -3,26 +3,31 @@ package main
 import (
     "bufio"
     "fmt"
+    "flag"
     "net/http"
 )
 
 func main() {
+    fromPtr := flag.String("from", "USD", "a string")
+    toPtr := flag.String("to", "INR", "a string")
+    storePtr := flag.String("store", "conv", "a string")
+    flag.Parse()
+    fmt.Println("from:", *fromPtr)
+    fmt.Println("to:", *toPtr)
+    fmt.Println("store in:", *storePtr)
+    fmt.Println("Arguments List:", flag.Args())
     base_url:="https://api.exchangeratesapi.io/latest?base="
-    usd:="USD"
-    stringg:=base_url+usd
-    fmt.Println(stringg)
-    resp, err := http.Get(base_url+usd)
+    resp, err := http.Get(base_url+*fromPtr)
     if err != nil {
         panic(err)
     }
     defer resp.Body.Close()
-
     fmt.Println("Response status:", resp.Status)
 
     scanner := bufio.NewScanner(resp.Body)
     for i := 0; scanner.Scan() && i < 5; i++ {
         fmt.Println(scanner.Text())
-    }
+    }T
 
     if err := scanner.Err(); err != nil {
         panic(err)
