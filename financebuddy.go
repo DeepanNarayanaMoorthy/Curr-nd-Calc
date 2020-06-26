2aea8bd20c9c9d65e420425a6d783f46b236d619
deepannarayanamoorthy <deepan06102000@gmail.com>
2020-06-26 12:17:47 +0530

    Handled json INSIDE CODE AND NOT AS A FILE, need more work

--- financebuddy.go
+++ financebuddy.go
@@ -3,19 +3,23 @@ package main
 import (
     "bufio"
     "fmt"
-    "io/ioutil"
+    //"io/ioutil"
     "flag"
     "net/http"
-    //"os"
+    "encoding/json"
 )
-
+type Currency struct{
+  Values map[string]float32
+  Base string
+  Datee string
+}
 func main() {
+    var curr Currency
     fromPtr := flag.String("from", "USD", "From what currency?")
     toPtr := flag.String("to", "INR", "To what currency?")
     storePtr := flag.String("store", "conv", "Where should I store the value")
     flag.Parse()
     fmt.Println(*toPtr+*storePtr)
-
     base_url:="https://api.exchangeratesapi.io/latest?base="
     resp, err := http.Get(base_url+*fromPtr)
     if err != nil {
@@ -31,7 +35,8 @@ func main() {
     for scanner.Scan(){
         currJson=currJson+scanner.Text()
     }
-
-    d1 := []byte(currJson)
-    ioutil.WriteFile("currJson.json", d1, 0644)
+    fmt.Println(currJson)
+    json.Unmarshal([]byte(currJson), &curr)
+    fmt.Println(curr.Base+" and "+curr.Datee)
+    fmt.Println(curr.Values["CNY"])
 }
