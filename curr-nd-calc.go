3a8cc83eb2f008c4b0f72361b6bbfd22d9016276
deepannarayanamoorthy <deepan06102000@gmail.com>
2020-06-27 16:47:07 +0530

    phone book add implemented

--- curr-nd-calc.go
+++ curr-nd-calc.go
@@ -7,24 +7,46 @@ import (
     "net/http"
     "encoding/json"
     "go/token"
-  	 "go/types"
+  	"go/types"
+    "io/ioutil"
+    "strings"
+    "os"
 )
 type Currency struct{
   Rates map[string]float32
   Base string `json:"base"`
   Date string `json:"date"`
 }
+
+type Contactt struct {
+    Name string `json:"Name"`
+    Number string `json:"Number"`
+    Relation string `json:"Relation"`
+}
+
+func checkFile(filename string) error {
+    _, err := os.Stat(filename)
+    if os.IsNotExist(err) {
+        _, err := os.Create(filename)
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
+
 func main() {
     var curr Currency
-    fromPtr := flag.String("from", "USD", "From what currency?")
-    toPtr := flag.String("to", "INR", "To what currency?")
+    fromPtr := flag.String("from", "", "From what currency?")
+    toPtr := flag.String("to", "", "To what currency?")
     calcPtr := flag.String("calc", "", "Simple Calculator")
+    contPtr := flag.String("cont", "", "Simple Contact Manager")
     flag.Parse()
     if *calcPtr != "" {
       fs := token.NewFileSet()
   		tv, _ := types.Eval(fs, nil, token.NoPos, *calcPtr)
   		fmt.Println(tv.Value)
-    } else {
+    } else if *fromPtr != "" {
       fmt.Println(*toPtr)
       base_url:="https://api.exchangeratesapi.io/latest?base="
       resp, err := http.Get(base_url+*fromPtr)
@@ -46,6 +68,41 @@ func main() {
       } else {
         fmt.Println(curr.Rates[*toPtr])
       }
+    } else if *contPtr != "" {
+      result := strings.Split(*contPtr, ",")
+      filename := "Contacts.json"
+      err := checkFile(filename)
+      if err != nil {
+          fmt.Println(err)
+      }
+
+      file, err := ioutil.ReadFile(filename)
+      if err != nil {
+          fmt.Println(err)
+      }
+
+      data := []Contactt{}
+
+      json.Unmarshal(file, &data)
+
+      newStruct := &Contactt{
+          Name: result[0],
+          Number: result[1],
+          Relation: result[2],
+      }
+
+      data = append(data, *newStruct)
+
+      dataBytes, err := json.Marshal(data)
+      if err != nil {
+          fmt.Println(err)
+      }
+
+      err = ioutil.WriteFile(filename, dataBytes, 0644)
+      if err != nil {
+          fmt.Println(err)
+      }
+
     }
 
 }
